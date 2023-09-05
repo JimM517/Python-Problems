a8f1c203b724885b3d11d71610dd2c72b8e55e78
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-04 20:16:31 -0400

    Completed isAnagram

--- Easy/contains_dup.py
+++ Easy/contains_dup.py
@@ -5,7 +5,7 @@
 # hash set solution
 class Solution:
     
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums: list[int]) -> bool:
         hashset = set()
         
         for n in nums:
