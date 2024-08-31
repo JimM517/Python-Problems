9fc1f0ee0e52978dfba5609e3f1aff9b8fb6780e
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-08-31 09:33:37 -0400

    gcd of strings

---
+++ LC75/Array_String/greatest_common_divisor.py
@@ -0,0 +1,22 @@
+# 1071. greatest common divisor of strings
+
+from math import gcd
+
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ''
+        
+        return str1[:gcd(len(str1), len(str2))]
+    
+
+
+
+str1 = "ABCABC"
+str2 = "ABC"
+
+sol = Solution()
+
+print(sol.gcdOfStrings(str1, str2))
+
