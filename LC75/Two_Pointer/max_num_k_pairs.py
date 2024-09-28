34d7c2658a33548b76b25bcda8b8f46aca0ac94a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-28 08:12:45 -0400

    max num k-pair nums

---
+++ LC75/Two_Pointer/max_num_k_pairs.py
@@ -0,0 +1,36 @@
+# 1679. max number of k-sum pairs
+from typing import List
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count = 0
+        
+        nums.sort()
+        
+        i, j = 0, len(nums) - 1
+        
+        while i < j:
+            if nums[i] + nums[j] == k:
+                count += 1
+                i += 1
+                j -= 1
+            elif nums[i] + nums[j] > k:
+                j -= 1
+            else:
+                i += 1
+        
+        return count
+    
+    
+    
+    
+    
+    
+    
+    
+    
+nums = [3, 1, 3, 4, 3]
+
+
+answer = Solution()
+print(answer.maxOperations(nums, 6))
