a43b4e5dc82a5a0dda7299070a5adbd4585b7f09
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-13 09:36:45 -0400

    contains dups two

---
+++ LC150/HashMap/contains_dups_two.py
@@ -0,0 +1,20 @@
+#219 contains duplicates two
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        result = {}
+        
+        
+        for i in range(len(nums)):
+            
+            num = nums[i]
+            
+            
+            if num in result and i - result[num] <= k:
+                return True
+            
+            result[num] = i
+            
+            
+        return False
+    
